4c7f107c71b9fb49ccc23bcc4f5e9a229aa7d1d0
DeadbcozIKilledit <jackson_rush@live.com.au>
2022-11-29 17:32:17 +0800

    initial commit of new repo, dev stage 3

---
+++ ReadMe.md
@@ -0,0 +1,1 @@
+Development stage part 3
---
+++ tailwind.config.js
@@ -0,0 +1,35 @@
+/** @type {import('tailwindcss').Config} */
+const colors = require('tailwindcss/colors')
+
+module.exports = {
+  mode: 'jit',
+  content: ['./*.html'],
+  theme: {
+    screens: {
+      sm: '480px',
+      md: '768px',
+      lg: '976px',
+      xl: '1440px'
+    },
+    extend: {
+      colors: {
+        brightRed: 'hsl(12, 88%, 59%)',
+        brightRedLight: 'hsl(13, 88%, 69%)',
+        brightRedSupLight: 'hsl(12, 88%, 95%)',
+        darkBlue: 'hsl(228, 39%, 23%)',
+        darkGrayishBlue: 'hsl(227, 12%, 61%)',
+        veryDarkBlue: 'hsl(233, 12%, 13%)',
+        veryPaleRed: 'hsl(13, 100%, 96%)',
+        veryLightGrey: 'hsl(0, 0%, 98%)',
+        regEarthWorm: '#c96d6c',
+        paleEarthWorm: '#d89796',
+        albinoEarthWorm: '#f0d8d8',
+        burntEarthWorm: '#531f1e',
+        lightEarthWorm: '#893331',
+        darkEarthWorm: '#0b0404',
+
+      }
+    },
+  },
+  plugins: [],
+}
